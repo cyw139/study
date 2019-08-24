5f7688a79ba14fa9bfcf64493bd85af65ab176b6
sam <75424121@qq.com>
2019-08-24 19:44:10 +0800

    lesson-8 install element-ui [ > vue add element ]

--- kkb/vue/vue-study/src/App.vue
+++ kkb/vue/vue-study/src/App.vue
@@ -1,45 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">
-        <!--<svg>-->
-          <!--<use xlink:href="#icon-route"></use>-->
-        <!--</svg>-->
-        <svg-icon icon-class="route" />
-        Home
-      </router-link> |
-      <router-link to="/about">About</router-link> |
-      <!--<router-link to="/login" v-if="!$store.state.isLogin">Login</router-link>-->
-      <!-- 精简写法 -->
-      <router-link to="/login" v-if="!isLogin">Login</router-link>
-      <span>{{loginState}}</span>
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
     </div>
-    <router-view/>
-    {{count}}
-    <button @click="onAdd">Add</button>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
-<script>
-  import {mapState, mapGetters} from 'vuex'
-  import kstore from "./kstore"
-  // import '@/icons/svg/route.svg'
 
-  export default {
-      computed: {
-          // 映射的字段
-          ...mapState(['isLogin']),
-          ...mapGetters(['loginState']),
-          count() {
-              return kstore.state.count
-          }
-      },
-      methods: {
-          onAdd() {
-              kstore.commit('add')
-          }
-      },
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
   }
+}
 </script>
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
---
+++ kkb/vue/vue-study/src/App_backup.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">
+        <!--<svg>-->
+        <!--<use xlink:href="#icon-route"></use>-->
+        <!--</svg>-->
+        <svg-icon icon-class="route" />
+        Home
+      </router-link> |
+      <router-link to="/about">About</router-link> |
+      <!--<router-link to="/login" v-if="!$store.state.isLogin">Login</router-link>-->
+      <!-- 精简写法 -->
+      <router-link to="/login" v-if="!isLogin">Login</router-link>
+      <span>{{loginState}}</span>
+    </div>
+    <router-view/>
+    {{count}}
+    <button @click="onAdd">Add</button>
+  </div>
+</template>
+<script>
+  import {mapState, mapGetters} from 'vuex'
+  import kstore from "./kstore"
+  // import '@/icons/svg/route.svg'
+
+  export default {
+    computed: {
+      // 映射的字段
+      ...mapState(['isLogin']),
+      ...mapGetters(['loginState']),
+      count() {
+        return kstore.state.count
+      }
+    },
+    methods: {
+      onAdd() {
+        kstore.commit('add')
+      }
+    },
+  }
+</script>
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
